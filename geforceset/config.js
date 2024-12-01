33f0d628727ba6d61aebba30ca1293080fac9c56
Anggazyy ZcoderX <anggazyyzxcy@gmail.com>
2024-12-01 11:07:24 +0800

    Create config.js

---
+++ geforceset/config.js
@@ -0,0 +1,14 @@
+global.owner = [
+  "6288804148639", //ganti nomor owner
+  "" //nomor owner kedua kalo ada
+]
+
+
+let fs = require('fs')
+let file = require.resolve(__filename)
+fs.watchFile(file, () => {
+fs.unwatchFile(file)
+console.log(`Update ${__filename}`)
+delete require.cache[file]
+require(file)
+})
